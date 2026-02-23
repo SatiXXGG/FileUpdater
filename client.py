6db54f068ea10f8923410f79e13d24a5e169e01c
Alejandro Vela <78871584+SatiXXGG@users.noreply.github.com>
2026-02-23 13:44:35 -0600

    feat: add .env support

--- client.py
+++ client.py
@@ -2,10 +2,31 @@
 import socket
 import json
 import os
+from time import sleep
 
-SERVER_IP = ""
+
+def load_env(path="data.env"):
+    if not os.path.exists(path):
+        return
+
+    with open(path) as f:
+        for line in f:
+            line = line.strip()
+
+            if not line or line.startswith("#"):
+                continue
+
+            key, value = line.split("=", 1)
+            os.environ[key.strip()] = value.strip()
+
+
+load_env()
+sleep(1)
+print("loaded env",os.environ.get("SERVER_IP"))
+SERVER_IP = os.environ["SERVER_IP"]
 PORT = int(input("Insert port: "))
 FOLDER = str(input("Insert destination folder path: "))
+
 
 def start_client():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
