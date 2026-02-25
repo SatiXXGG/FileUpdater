3724ea78a6207134934f8df2825e33a32feab3c8
Alejandro Vela <78871584+SatiXXGG@users.noreply.github.com>
2026-02-24 19:12:37 -0600

    feat: server test

--- server.py
+++ server.py
@@ -1,53 +1,20 @@
-
-import socket
-import os
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
+from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 
-HOST = "0.0.0.0"
-PORT = int(input("Insert port: "))
-FOLDER = str(input("Insert project folder path: "))
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-Type", "application/json")
+        self.end_headers()
 
-clients = []
+        response = {
+            "status": "ok",
+            "message": "Servidor funcionando 🔥",
+            "port": 6969
+        }
 
-class ChangeHandler(FileSystemEventHandler):
-    def on_modified(self, event):
-        if event.is_directory:
-            return
-        file_path = event.src_path
+        self.wfile.write(json.dumps(response).encode())
 
-        try:
-            with open(file_path, "rb") as f:
-                content = f.read()
-
-                data = {
-                    "path": os.path.relpath(file_path, FOLDER),
-                    "content": content.decode("latin1")
-                }
-                message = json.dumps(data).encode("latin1")
-                for client in clients:
-                    client.sendall(message)
-
-                print("File modified:", file_path)
-        except Exception as e:
-            print("Error:", e)
-
-
-def start_server():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((HOST, PORT))
-    server.listen()
-    print("Server is starting...")
-
-    while True:
-        conn, adrr = server.accept()
-        clients.append(conn)
-        print("Client connected:", adrr)
-
-
-if __name__ == "__main__":
-    observer = Observer()
-    observer.schedule(ChangeHandler(), FOLDER, recursive=True)
-    observer.start()
-    start_server()
+server = HTTPServer(("0.0.0.0", 6969), Handler)
+print("Servidor corriendo en puerto 6969...")
+server.serve_forever()
